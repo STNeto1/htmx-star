2e7929fe34cdd92cb53d395a05ddd4a9916ece1c
STNeto1 <gfreitasneto18@gmail.com>
2023-09-02 23:12:46 -0300

    kinda working

--- grid.go
+++ grid.go
@@ -3,14 +3,19 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math"
+	"math/rand"
 )
 
 type GridElement struct {
-	X int
-	Y int
-	F float32
-	G float32
-	H float32
+	X         int
+	Y         int
+	F         float32
+	G         float32
+	H         float32
+	Neighbors []*GridElement
+	Previous  *GridElement
+	Wall      bool
 }
 
 func (elem *GridElement) Compare(toCompare *GridElement) bool {
@@ -18,6 +23,22 @@ func (elem *GridElement) Compare(toCompare *GridElement) bool {
 }
 
 func (elem *GridElement) Label(grid *Container) string {
+	if elem.Wall {
+		return "W"
+	}
+
+	if elem.Compare(grid.Start) {
+		return "S"
+	}
+
+	if elem.Compare(grid.End) {
+		return "E"
+	}
+
+	if grid.InPath(elem) {
+		return "P"
+	}
+
 	for _, inner := range grid.OpenSet {
 		if elem.Compare(inner) {
 			return "O"
@@ -30,16 +51,61 @@ func (elem *GridElement) Label(grid *Container) string {
 		}
 	}
 
-	return "G"
+	return " "
+}
+
+func (elem *GridElement) CalculareNeighbors(grid *Container) {
+	cols := len(grid.Grid)
+	rows := len(grid.Grid[0])
+
+	if elem.X < cols-1 {
+		elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X+1][elem.Y])
+	}
+
+	if elem.X > 0 {
+		elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X-1][elem.Y])
+	}
+
+	if elem.Y < rows-1 {
+		elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X][elem.Y+1])
+	}
+
+	if elem.Y > 0 {
+		elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X][elem.Y-1])
+	}
+
+	// Diagonal
+	// if elem.X > 0 && elem.Y > 0 {
+	// 	elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X-1][elem.Y-1])
+	// }
+	//
+	// if elem.X < cols-1 && elem.Y > 0 {
+	// 	elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X+1][elem.Y-1])
+	// }
+	//
+	// if elem.X > 0 && elem.Y < cols-1 {
+	// 	elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X-1][elem.Y+1])
+	// }
+	//
+	// if elem.X < rows-1 && elem.Y < cols-1 {
+	// 	elem.Neighbors = append(elem.Neighbors, grid.Grid[elem.X+1][elem.Y+1])
+	// }
+}
+
+func (elem *GridElement) UpdateHeuristics(to *GridElement) {
+	elem.H = float32(math.Abs(float64(elem.X-to.X)) + math.Abs(float64(elem.Y-to.Y)))
 }
 
 func NewElement(x, y int) *GridElement {
 	return &GridElement{
-		X: x,
-		Y: y,
-		F: 0,
-		G: 0,
-		H: 0,
+		X:         x,
+		Y:         y,
+		F:         0,
+		G:         0,
+		H:         0,
+		Neighbors: []*GridElement{},
+		Previous:  nil,
+		Wall:      rand.Float64() < 0.3,
 	}
 }
 
@@ -49,6 +115,7 @@ type Container struct {
 	ClosedSet []*GridElement
 	Start     *GridElement
 	End       *GridElement
+	Path      []*GridElement
 }
 
 func NewContainer(cols, rows int) *Container {
@@ -66,24 +133,127 @@ func NewContainer(cols, rows int) *Container {
 	start := grid[0][0]
 	end := grid[cols-1][rows-1]
 
-	openSet := []*GridElement{start}
-	closedSet := make([]*GridElement, 0)
+	start.Wall = false
+	end.Wall = false
 
-	return &Container{
+	openSet := []*GridElement{start}
+	closedSet := []*GridElement{}
+
+	container := &Container{
 		Grid:      grid,
 		OpenSet:   openSet,
 		ClosedSet: closedSet,
 		Start:     start,
 		End:       end,
+		Path:      []*GridElement{},
 	}
+
+	for _, col := range container.Grid {
+		for _, elem := range col {
+			elem.CalculareNeighbors(container)
+		}
+	}
+
+	return container
 }
 
 func (c *Container) Tick() error {
-	if len(c.ClosedSet) == 0 {
+	if len(c.OpenSet) == 0 {
 		return errors.New("no solution")
 	}
 
+	lowest := c.LowestInOpenSet()
+	if lowest.Compare(c.End) {
+		c.Backtrack()
+
+		return errors.New("end reached")
+	}
+
+	c.ClosedSet = append(c.ClosedSet, lowest)
+	c.RemoveFromOpenSet(lowest)
+
+	for _, neighbor := range lowest.Neighbors {
+		if c.InClosedSet(neighbor) || neighbor.Wall {
+			continue
+		}
+
+		tmpG := lowest.G + 1
+
+		if c.InOpenSet(neighbor) {
+			if tmpG < neighbor.G {
+				// tmpG = neighbor.G
+				neighbor.G = tmpG
+			}
+		} else {
+			neighbor.G = tmpG
+			c.OpenSet = append(c.OpenSet, neighbor)
+		}
+
+		if tmpG == neighbor.G {
+			neighbor.UpdateHeuristics(c.End)
+			neighbor.F = neighbor.G + neighbor.H
+			neighbor.Previous = lowest
+		}
+	}
+
 	return nil
+}
+
+func (c *Container) RemoveFromOpenSet(toRemove *GridElement) {
+	result := []*GridElement{}
+
+	for _, elem := range c.OpenSet {
+		if !elem.Compare(toRemove) {
+			result = append(result, elem)
+		}
+	}
+
+	c.OpenSet = result
+}
+
+func (c *Container) InClosedSet(toCheck *GridElement) bool {
+	for _, elem := range c.ClosedSet {
+		if elem.Compare(toCheck) {
+			return true
+		}
+
+	}
+
+	return false
+}
+
+func (c *Container) InOpenSet(toCheck *GridElement) bool {
+	for _, elem := range c.OpenSet {
+		if elem.Compare(toCheck) {
+			return true
+		}
+
+	}
+
+	return false
+}
+
+func (c *Container) InPath(toCheck *GridElement) bool {
+	for _, elem := range c.Path {
+		if elem.Compare(toCheck) {
+			return true
+		}
+
+	}
+
+	return false
+}
+
+func (c *Container) LowestInOpenSet() *GridElement {
+	root := c.OpenSet[0]
+
+	for _, elem := range c.OpenSet {
+		if !root.Compare(elem) && elem.F < root.F {
+			root = elem
+		}
+	}
+
+	return root
 }
 
 func (c *Container) Print() {
@@ -92,5 +262,17 @@ func (c *Container) Print() {
 			fmt.Printf("%s ", row.Label(c))
 		}
 		fmt.Println()
+	}
+}
+
+func (c *Container) Backtrack() {
+	start := c.End
+	for true {
+		if start.Previous == nil {
+			break
+		}
+
+		c.Path = append(c.Path, start.Previous)
+		start = start.Previous
 	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	container := NewContainer(5, 5)
+	container := NewContainer(25, 25)
+
+	for true {
+		// container.Print()
+		// fmt.Println("===")
+
+		if err := container.Tick(); err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+
+		// time.Sleep(time.Millisecond * 500)
+	}
 
 	container.Print()
 }
